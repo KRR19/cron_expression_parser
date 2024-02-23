e0486f3f32e99744edfc3da027146102587658aa
Roman Kazarov <roman.kazarov@gett.com>
2024-02-23 16:21:22 +0100

    add expresion split

--- cmd/internal/cronparser/service.go
+++ cmd/internal/cronparser/service.go
@@ -1,5 +1,10 @@
 package cronparser
 
+import (
+	"errors"
+	"strings"
+)
+
 type service struct {
 }
 
@@ -10,6 +15,11 @@ func New() *service {
 func (s *service) Parse(expression string) (*CronFields, error) {
 	if !s.isValidCronExpression(expression) {
 		return nil, ErrInvalidExpression
+	}
+
+	fields := strings.Fields(expression)
+	if len(fields) != 7 {
+		return nil, errors.New("invalid cron string format")
 	}
 	return &CronFields{}, nil
 }
